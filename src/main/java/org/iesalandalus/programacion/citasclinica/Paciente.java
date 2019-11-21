20b308f5474dee48fd37bd15198fc7477b0f8e95
marta1196 <34567942+marta1196@users.noreply.github.com>
2019-11-21 17:13:04 +0100

    Creacion de la clase Paciente y sus atributos

---
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Paciente.java
@@ -0,0 +1,12 @@
+package org.iesalandalus.programacion.citasclinica;
+
+public class Paciente {
+
+	private static final String ER_DNI="([0-9]{8})([A-Za-z])";
+	private static final String ER_TELEFONO="[96]?[0-9]{8}";
+
+	private String nombre;
+	private String dni;
+	private String telefono;
+
+}
